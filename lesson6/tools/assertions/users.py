b5ed62a6f4b3d652a4e2c5d654fbe64e73382658
MaksimPonomarev <160363705+MaksimPonomarev@users.noreply.github.com>
2026-04-08 06:33:27 +0300

    фикстуры

--- lesson6/tools/assertions/users.py
+++ lesson6/tools/assertions/users.py
@@ -1,4 +1,5 @@
-from lesson6.clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from lesson6.clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, UserSchema, \
+    GetUserResponseSchema
 from lesson6.tools.assertions.base import assert_equal
 
 
@@ -14,3 +15,25 @@ def assert_create_user_response(request: CreateUserRequestSchema, response: Crea
     assert_equal(response.user.last_name, request.last_name, "last_name")
     assert_equal(response.user.first_name, request.first_name, "first_name")
     assert_equal(response.user.middle_name, request.middle_name, "middle_name")
+
+
+def assert_user(actual: UserSchema, expected: UserSchema):
+    """
+    Проверяет корректность данных пользователя:
+    :param actual:
+    :param expected:
+    """
+    assert_equal(actual.id, expected.id, name="id")
+    assert_equal(actual.email, expected.email, name="email")
+    assert_equal(actual.last_name, expected.last_name, name="last_name")
+    assert_equal(actual.first_name, expected.first_name, name="first_name")
+    assert_equal(actual.middle_name, expected.middle_name, name="middle_name")
+
+
+def assert_get_user_response(get_user_response: GetUserResponseSchema,create_user_response: CreateUserResponseSchema):
+    """
+    Проверяет, что данные пользователя при создании и при запросе совпадают
+    :param get_user_response:
+    :param create_user_response:
+    """
+    assert_user(get_user_response.user, create_user_response.user)
